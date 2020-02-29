2e3e4a5e32c46953bd3d805f4079d9e78805b536
kevinluo201 <kevinluo201@gmail.com>
2020-02-29 23:10:54 +0800

    modify index page

--- vendor/assets/javascripts/init.js
+++ vendor/assets/javascripts/init.js
@@ -441,7 +441,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 
 		animateSpan.typed({
-			strings: ["Freelancer", "Web Developer", "Rubyist", "System Architect"],
+			strings: ["Freelancer", "Web Developer", "Full-stack Engineer", "Rubyist", "System Architect"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3

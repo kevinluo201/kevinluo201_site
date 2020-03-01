310bbea6aae1212dd3e3a3c7d321c0c76ccce226
kevinluo201 <kevinluo201@gmail.com>
2020-03-01 23:29:06 +0800

    add cv link remove logo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def cv
+    send_file "public/2019_KaiWen_Luo_cv.pdf", type: 'image/png', status: 202
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   resources :projects, only: [:show]
+
+  get 'cv', to: 'pages#cv', as: :cv
 end
